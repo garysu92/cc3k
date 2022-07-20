5ebe500e67605b02e729575e05df240e41721e65
WenHaozhan <wenhao_zhan@hotmail.com>
2022-07-19 21:14:35 -0400

    changes

--- textdisplay.h
+++ textdisplay.h
@@ -1,9 +1,12 @@
 #ifndef TEXTDISPLAY_H
-
 #define  TEXTDISPLAY_H
 
-class  TextDisplay{
+#include <iostream>
+#include <vector>
 
+class  TextDisplay: Observer<> {
+    vector<vector<char>> floorGrid;
+    friend std::ostream &<<(std::ostream &out, const TextDisplay t);
 };
 
 #endif
