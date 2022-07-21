36bbcd2d4bdc60204fad68d3837bd644c56f5056
WenHaozhan <wenhao_zhan@hotmail.com>
2022-07-20 22:54:33 -0400

    changes

--- dungeon.cc
+++ dungeon.cc
@@ -1,9 +1,18 @@
 #include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
 #include "dungeon.h"
 
 using namespace std;
 
-Dungeon::Dungeon(string fileName, int curFloor, int numFloors) : fileName{fileName}, curFloor{curFloor}, numFloors{numFloors} {}
+Dungeon::Dungeon(string fileName, int curFloor, int numFloors) : fileName{fileName}, curFloor{curFloor}, numFloors{numFloors} {
+    ofstream file;
+    vector<vector<char>> v;
+    v.emplace_back();
+    
+    // REMEMBER to catch potential erros
+}
 
 int Dungeon::get_curFloor() {
     return this->curFloor;
