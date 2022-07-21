9e4762ee92805d8f9c3ecb57a7cbd59e6a1b6476
garysu92 <garysu929292@gmail.com>
2022-07-21 18:18:34 -0400

    fixed cell.h

--- Cells/cell.h
+++ Cells/cell.h
@@ -15,7 +15,7 @@ class Cell {
     unique_ptr<Item> item;
     char symbolRep;
     bool isEffWall; // Dictates whether or not the Cell effectively behaves as a wall, meaning the PC/Enemy cannot move through it.
-    bool isStair = false; // Does this need be defined here?
+    bool isStair; // Does this need be defined here?
 
     public:
         // Constructor
