f40bb52180a16bf69fe6a3d57183402dbc1dac17
Paul Coones <pcoones@uwaterloo.ca>
2022-07-24 14:26:40 -0400

    New action display classes

--- Display/actionDisplay.cc
+++ Display/actionDisplay.cc
@@ -1,1 +1,24 @@
+#include <string>
+#include "Entities/playablecharacter.h"
 
+Actiondisplay::Actiondisplay(PlayableCharacter * newPC) : PC{newPC} {}
+
+void set_curAction(string newAction) {
+    this->curAction = newAction;
+}
+
+void set_PC(PlayableCharacter * newPC) {
+    this->PC = newPC;
+}
+
+string get_curAction() {
+    return this->curAction;
+}
+
+PlayableCharacter * get_PC() {
+    return this->PC;
+}
+        
+void printActionDisplay() {
+    
+}
--- Display/actionDisplay.h
+++ Display/actionDisplay.h
@@ -1,1 +1,27 @@
+#ifndef ACTIONDISPLAY_H
+#define ACTIONDISPLAY_H
 
+#include <string>
+#include "Entities/playablecharacter.h"
+
+class Actiondisplay {
+    PlayableCharacter * PC; 
+    string curAction;
+    
+    public:
+        // Constructor
+        Actiondisplay();
+
+        // Setters
+        void set_curAction(string newAction);
+        void set_PC(PlayableCharacter * newPC);
+
+        // Getters
+        string get_curAction();
+        PlayableCharacter * get_PC();
+
+        // Misc
+        void printActionDisplay();
+};
+
+#endif
