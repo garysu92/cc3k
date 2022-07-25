cd164befdd08b50ccc6c6ad2b6442d3abdba0d79
garysu92 <garysu929292@gmail.com>
2022-07-24 20:10:11 -0400

    g

--- floor.cc
+++ floor.cc
@@ -432,7 +432,7 @@ void Floor::movePC(Direction d) {
     Posn pos = getCoords(d);
     int cx = pos.x;
     int cy = pos.y;
-	if (cy < content.size() && cx < content[0].size() && !content[cy][cx]->hasEnemy() && \
+	if (cy < content.size() && cx < content[0].size() && !content[cy][cx]->hasEnemy() && !content[cy][cx]->hasPotion() && \
         (content[cy][cx]->getsymbolRep() == '.' || content[cy][cx]->getsymbolRep() == '+' \
         || content[cy][cx]->getsymbolRep() == '#')) {
         content[pcLocation.y][pcLocation.x]->clear();
