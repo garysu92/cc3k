68941ec1d68d0f3575b9ce9a84c649b214423cfb
WenHaozhan <wenhao_zhan@hotmail.com>
2022-07-25 18:39:58 -0400

    c

--- Enemies/goblin.cc
+++ Enemies/goblin.cc
@@ -1,4 +1,3 @@
-#include <string>
 #include "goblin.h"
 #include "../Entities/enemy.h"
 #include <string>
@@ -11,10 +10,6 @@ bool Goblin::isHostile() {
     return true;
 }
 
-<<<<<<< HEAD
-std::string Goblin::getType() const {
-=======
 string Goblin::getType() const {
->>>>>>> d670a007c2f947c7fcc7b76b33f6860fd501b710
     return "Goblin (N)";
 }
