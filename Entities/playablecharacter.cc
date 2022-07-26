9ee3f155ffa33fdc99ca0bb35697909efd257f22
garysu92 <garysu929292@gmail.com>
2022-07-26 16:43:05 -0400

    h

--- Entities/playablecharacter.cc
+++ Entities/playablecharacter.cc
@@ -25,7 +25,7 @@ void PlayableCharacter::takeDmg(Enemy *enemy) {
     //cout << "PC currently has " << this->getHP() << " hp. ";
     int dmg = ceil((100.0 / (100.0 + getDefense())) * enemy->getAttack());
     if (this->hasBarrierSuit) {
-        dmg = ceil(dmg/2);
+        dmg = ceil(dmg / 2.0);
     }
     setHP(max(0, this->getHP() - dmg));
     if (this->getHP() == 0) {
